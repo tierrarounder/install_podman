80adb810b4e7b02ee3783f5b04e102b5f6793338
Tierra Rounder <105559398+tierrarounder@users.noreply.github.com>
2022-06-17 13:58:48 -0500

    Add files via upload
    bash script to install podman.  Debian or Ubuntu.

---
+++ install_podman.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade && sudo apt full-upgrade
+#
+source /etc/os-release
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+sudo apt update
+sudo apt -y install podman
+podman info
+
